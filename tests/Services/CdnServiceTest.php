1d9204b54aff0f118c1b98a7cc6e584c437ce99a
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-29 17:47:36 +0430

    Add services tests

---
+++ tests/Services/CdnServiceTest.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mohammadv184\ArvanCloud\Laravel\Tests\Services;
+
+use Mohammadv184\ArvanCloud\Laravel\Tests\TestCase;
+
+class CdnServiceTest extends TestCase
+{
+    public function test()
+    {
+        $this->assertTrue(true);
+    }
+}
