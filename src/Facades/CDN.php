26461c0ed30b981b98e4112dc359ec77ec2230f3
Mohammad Abbasi <mohammad.v184@gmail.com>
2021-07-29 17:11:07 +0430

    Add phpdoc

--- src/Facades/CDN.php
+++ src/Facades/CDN.php
@@ -2,7 +2,19 @@
 
 namespace Mohammadv184\ArvanCloud\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
+use Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Cache;
+use Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Dns;
+use Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Domain;
+use Mohammadv184\ArvanCloud\Services\Cdn\Endpoints\Ssl;
 
+/**
+ * Class CDN
+ * @method static Cache  cache(string $domain = null)
+ * @method static Dns    dns(string $domain = null)
+ * @method static Domain domain(string $domain = null)
+ * @method static Ssl    ssl(string $domain = null)
+ * @package Mohammadv184\ArvanCloud\Laravel\Facades
+ */
 class CDN extends Facade
 {
     protected static function getFacadeAccessor()
